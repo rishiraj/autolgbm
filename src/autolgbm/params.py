d9359839230623cc6767d17bb6c5a84a50fe07c4
Rishiraj Acharya <44090649+rishiraj@users.noreply.github.com>
2022-02-13 03:58:08 +0530

    Update params.py

--- src/autolgbm/params.py
+++ src/autolgbm/params.py
@@ -1,10 +1,6 @@
 def get_params(trial, model_config):
     params = {
         "learning_rate": trial.suggest_float("learning_rate", 1e-2, 0.25, log=True),
-        "reg_lambda": trial.suggest_float("reg_lambda", 1e-8, 100.0, log=True),
-        "reg_alpha": trial.suggest_float("reg_alpha", 1e-8, 100.0, log=True),
-        "subsample": trial.suggest_float("subsample", 0.1, 1.0),
-        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.1, 1.0),
         "max_depth": trial.suggest_int("max_depth", 1, 9),
         "early_stopping_rounds": trial.suggest_int("early_stopping_rounds", 100, 500),
         "n_estimators": trial.suggest_categorical("n_estimators", [7000, 15000, 20000]),
